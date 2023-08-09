367f32b438614134dabdd7122601bfe9748931dc
Сидоров Дмитрий Евгеньевич <Dzmitry.Sidarau@alfa-bank.by>
2023-08-09 22:49:47 +0300

    Create saved gist schema

---
+++ lib/gist_clone/gists/saved_gist.ex
@@ -0,0 +1,21 @@
+defmodule GistClone.Gists.SavedGist do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "saved_gists" do
+
+    field :user_id, :binary_id
+    field :gist_id, :binary_id
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(saved_gist, attrs) do
+    saved_gist
+    |> cast(attrs, [])
+    |> validate_required([])
+  end
+end
